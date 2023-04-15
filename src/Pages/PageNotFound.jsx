e4e6244320131134a6147adbeca5a716199000c1
SanderGeuens <sander.geuens@student.hogent.be>
2023-04-15 17:37:37 +0200

    aanpassen PageNotFound

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -1,9 +1,14 @@
+import { useLocation } from "react-router";
 
 
 export default function PageNotFound() {
+
+    const {pathName} = useLocation;
+
     return (
         <div>
             <div>Page not found!</div>
+            <div>There is no page with url {pathName}</div>
         </div>
     )
 }
