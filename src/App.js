e12bf2a71c95c7587dc33b42a2c906dfd655bd51
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-04-22 09:55:04 +0200

    Remove old shoppingcart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import {
-  HomePage, NotFoundPage, ProductOverviewPage, ProductsPage, ProfilePage, ShoppingCartPage,
+  HomePage, NotFoundPage, ProductOverviewPage, ProductsPage, ProfilePage,
 } from './pages';
 import ScrollToTop from './Contexts/ScrollToTop';
 import Footer from './Components/Footer';
 import Navibar from './Components/Navibar/Navibar';
-import ThankyouScreen from './Components/shoppingCart/ThankyouScreen'
-import ShoppingCart from './Components/shoppingCart/ShoppingCart';
-
 
 function App() {
   return (
@@ -21,8 +18,6 @@ function App() {
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/products/product/:id" element={<ProductOverviewPage />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/shoppingCart" element={<ShoppingCart />} />
-        <Route path="/thankyouScreen" elelemnt={<ThankyouScreen/>}/>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
