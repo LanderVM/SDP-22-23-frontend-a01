0c2576e65c602de84f6c8957b781856baaf993c1
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-04-23 21:45:27 +0200

    Retrieve Order from backend with Tracking Code
    Add orderTrackingService
    Implement orderTrackingService in TrackingInput

--- src/Components/Tracking/TrackingInput.jsx
+++ src/Components/Tracking/TrackingInput.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './trackingInput.scss';
 import {
   Input, Form, Col, Row, Button,
 } from 'antd';
 import { useForm } from 'antd/es/form/Form';
+import useOrderTracker from '../../api/orderTrackingService';
 
 export default function TrackingInput() {
   const [form] = useForm();
-  const onFinish = (values) => {
-    console.log(`Form values: ${JSON.stringify(values)}`);
+  const [tracker, setTracker] = useState(null);
+  const orderTrackingApi = useOrderTracker();
+
+  const onFinish = async (values) => {
+    const toTrackOrder = await orderTrackingApi.getTrackingStatus(values);
+    setTracker(toTrackOrder);
   };
 
   return (
     <div className="delaware-tracking-page">
+      {/* TODO add tracking page */}
+      {/* TODO add error component */}
+      {/* TODO add loading component */}
+      <p>{!tracker ? 'aaa' : tracker.items.delivery_country}</p>
       <div className="delaware-tracking-head">
         <h1>Order&nbsp;Tracking&nbsp;Page</h1>
         <p>
@@ -23,12 +32,12 @@ export default function TrackingInput() {
       <Form form={form} onFinish={onFinish}>
         <Row gutter={16}>
           <Col span={12}>
-            <Form.Item name="tracking-code" label="Tracking code" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }} rules={[{ required: true }]}>
+            <Form.Item name="trackingCode" label="Tracking code" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }} rules={[{ required: true }]}>
               <Input />
             </Form.Item>
           </Col>
           <Col span={12}>
-            <Form.Item name="verification-code" label="Extra Verification code" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }} rules={[{ required: true }]}>
+            <Form.Item name="verificationCode" label="Extra Verification code" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }} rules={[{ required: true }]}>
               <Input />
             </Form.Item>
           </Col>
---
+++ src/api/orderTrackingService.js
@@ -0,0 +1,31 @@
+import {
+  useAuth0,
+} from '@auth0/auth0-react';
+import axios from 'axios';
+import {
+  useCallback,
+} from 'react';
+
+const baseUrl = `${process.env.REACT_APP_API_URL}/order`;
+
+export default function useOrderTracker() {
+  const {
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  const getTrackingStatus = useCallback(async ({ trackingCode, verificationCode }) => {
+    const token = await getAccessTokenSilently();
+    const {
+      data,
+    } = await axios.get(`${baseUrl}/?trackAndTraceCode=${trackingCode}&verificatiecode=${verificationCode}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return data;
+  }, [getAccessTokenSilently]);
+
+  return {
+    getTrackingStatus,
+  };
+}
