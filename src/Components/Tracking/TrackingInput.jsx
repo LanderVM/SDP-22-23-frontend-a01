a362ba509f507bdd0e79e27a69e707ef0b2bb78f
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-04-23 21:13:27 +0200

    Add working input for TrackingInput

--- src/Components/Tracking/TrackingInput.jsx
+++ src/Components/Tracking/TrackingInput.jsx
@@ -3,8 +3,14 @@ import './trackingInput.scss';
 import {
   Input, Form, Col, Row, Button,
 } from 'antd';
+import { useForm } from 'antd/es/form/Form';
 
 export default function TrackingInput() {
+  const [form] = useForm();
+  const onFinish = (values) => {
+    console.log(`Form values: ${JSON.stringify(values)}`);
+  };
+
   return (
     <div className="delaware-tracking-page">
       <div className="delaware-tracking-head">
@@ -14,15 +20,15 @@ export default function TrackingInput() {
           and the extra verification code you received after ordering in the second box
         </p>
       </div>
-      <Form>
+      <Form form={form} onFinish={onFinish}>
         <Row gutter={16}>
           <Col span={12}>
-            <Form.Item label="Tracking code" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
+            <Form.Item name="tracking-code" label="Tracking code" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }} rules={[{ required: true }]}>
               <Input />
             </Form.Item>
           </Col>
           <Col span={12}>
-            <Form.Item label="Extra Verification code" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
+            <Form.Item name="verification-code" label="Extra Verification code" labelCol={{ span: 24 }} wrapperCol={{ span: 24 }} rules={[{ required: true }]}>
               <Input />
             </Form.Item>
           </Col>
