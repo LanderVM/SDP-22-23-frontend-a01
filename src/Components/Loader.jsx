dfb6282ddbb17bfbcacf1c750ab9e529f50f1e4b
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-04-26 16:42:20 +0200

    Modify Loader to use ant design instead of bootstrap

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
+import { Spin } from 'antd';
 
 export default function Loader({ loading }) {
   if (loading) {
     return (
-      <div style={{ textAlign: 'center' }}>
-        <div className="spinner-border" data-cy="loading">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <p>Loading...</p>
-      </div>
+      <Spin tip="Loading.." size="large" />
     );
   }
 
