9e803a04053642029f79bf8640fb3a9177324e4a
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-05-04 00:01:06 +0200

    Fix "Orders" button from dropdown now navigates to orders page

--- src/Components/Navibar/OrderButton.jsx
+++ src/Components/Navibar/OrderButton.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { ShoppingCartOutlined } from '@ant-design/icons';
+import { NavLink } from 'react-router-dom';
 
 export default function OrderButton() {
   return (
-    <div className="delaware-dropdown-button">
+    <NavLink to="/orders" className="user-item delaware-dropdown-button">
       <ShoppingCartOutlined twoToneColor="#FFFFFF" style={{ fontSize: '250%' }} />
       &nbsp;Orders
-    </div>
+    </NavLink>
   );
 }
