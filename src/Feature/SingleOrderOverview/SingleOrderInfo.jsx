954f661634329a5a393150958ee4f6d7adb76f2d
THOMAS LISSENS <THOMAS.LISSENS@student.hogent.be>
2023-05-08 23:27:17 +0200

    update singleOrderInfo

--- src/Feature/SingleOrderOverview/SingleOrderInfo.jsx
+++ src/Feature/SingleOrderOverview/SingleOrderInfo.jsx
@@ -1,7 +1,7 @@
 import {
   Button, Col, List, Modal, Row,
 } from 'antd';
-import '../OrderOverview/orderInfo.scss';
+// import '../OrderOverview/orderInfo.scss';
 import moment from 'moment';
 import useBreakpoint from 'antd/es/grid/hooks/useBreakpoint';
 import React, { useState } from 'react';
@@ -10,11 +10,15 @@ import getStatusAsString from '../../utils';
 import SingleProductInfo from './SingleProductInfo';
 import ChangeAddress from './ChangeAddress';
 
+const size0 = 0;
+
 export default function SingleOrderInfo({ order }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { lg } = useBreakpoint();
-  const fontSizeName = lg ? '24px' : '16px';
-  const fontSizeDesc = lg ? '18px' : '14px';
+  const fontSizeName = lg ? '36px' : '24px';
+  const fontSizeDesc = lg ? '24px' : '18px';
+  const fontSizeMini = lg ? '18px' : '14px';
+  const positionChangePackageMF = lg ? 'absolute' : 'relative';
 
   /* const calculateTotalCost = useMemo(() => {
     const totalCost = order.product_list.map((product) => product.product_count * product.original_acquisition_price).reduce(
@@ -66,24 +70,24 @@ export default function SingleOrderInfo({ order }) {
           <h1 style={{ fontSize: fontSizeName }}>Order details</h1>
 
         </Row>
-        <Row style={{ display: 'inline-flex', fontSize: fontSizeDesc, columnGap: '40px' }}>
-          <Col xs={{ span: 12 }} lg={{ span: 4 }}>
-            <h1>Delivery adress</h1>
-            <div>
-              <p>
+        <Row style={{ fontSize: fontSizeDesc }}>
+          <Col xs={{ span: 24 }} lg={{ span: 6 }}>
+            <h1 style={{ fontSize: fontSizeDesc }}>Delivery adress</h1>
+            <div style={{ fontSize: fontSizeMini }}>
+              <div>
                 {order.order_info.delivery_street}
-&nbsp;
+                  &nbsp;
                 {order.order_info.delivery_house_number}
-              </p>
-              <p>
+              </div>
+              <div>
                 {order.order_info.delivery_postal_code}
-                    &nbsp;
+                  &nbsp;
                 {order.order_info.delivery_city}
-              </p>
-              <p>
+              </div>
+              <div>
                 {order.order_info.delivery_country}
-              </p>
-              <Button type="link" onClick={showModal}> &#62;&nbsp;Change address</Button>
+              </div>
+              <Button type="link" onClick={showModal} style={{ padding: size0, fontSize: fontSizeMini }}> &#62;&nbsp;Change address</Button>
               <Modal
                 title="Change address"
                 open={isModalOpen}
@@ -94,27 +98,37 @@ export default function SingleOrderInfo({ order }) {
               </Modal>
             </div>
           </Col>
-          <Col xs={{ span: 12 }} lg={{ span: 4 }}>
-            <h1>Packaging</h1>
-            <div>{order.order_info.name}</div>
-            <Button type="link" onClick={showModal}> &#62;&nbsp;Change packaging</Button>
+          <Col xs={{ span: 24 }} lg={{ span: 8 }}>
+            <h1 style={{ fontSize: fontSizeDesc }}>Packaging</h1>
+            <div style={{ fontSize: fontSizeMini }}>{order.order_info.name}</div>
+            <Button
+              style={{
+                position: positionChangePackageMF, bottom: size0, padding: size0, fontSize: fontSizeMini,
+              }}
+              type="link"
+              onClick={showModal}
+            >
+              &#62;&nbsp;Change packaging
+            </Button>
           </Col>
           <Col xs={{ span: 12 }} lg={{ span: 4 }}>
-            <h1>Total amount: </h1>
-            <NavLink to="/track">
+            <h1 style={{ fontSize: fontSizeDesc }}>Total amount: </h1>
+            <NavLink
+              to="/track"
+              style={{
+                position: positionChangePackageMF, bottom: size0, height: '26px', fontSize: fontSizeMini, color: '#1677ff',
+              }}
+            >
               &#62;&nbsp;Track order
             </NavLink>
-
           </Col>
-          <Col xs={{ span: 12 }} lg={{ span: 4 }}>
-            <h1>
+          <Col xs={{ span: 12 }} lg={{ span: 6 }}>
+            <h1 style={{ fontSize: fontSizeDesc }}>
               €&nbsp;
               444444
             </h1>
-
           </Col>
         </Row>
-
       </Col>
     </Row>
   );
