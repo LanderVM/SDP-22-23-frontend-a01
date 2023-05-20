241857be71ebc1b15a0d909e21649e03691f7347
SanderGeuens <sander.geuens@student.hogent.be>
2023-05-21 01:43:36 +0200

    afwerken test notifications

--- cypress/e2e/notificationsTest.cy.js
+++ cypress/e2e/notificationsTest.cy.js
@@ -45,14 +45,25 @@ describe('notifications test', () => {
     cy.url().should('equal', 'http://localhost:3000/notifications');
   });
 
-  it('test notifications from new to unread',()=>{
+  it('test notifications overview',()=>{
     cy.visit('http://localhost:3000/notifications');
     cy.wait(500);
+    cy.get('[data-cy=notification]').should("have.length", 8);
+    cy.get('[data-cy=notificationDate]').eq(0).contains('2023-05-17');
+    cy.get('[data-cy=notificationMessage]').eq(0).contains('Your order has been processed');
     cy.get('[data-cy=notificationStatus]').eq(0).contains('new');
-    cy.visit('http://localhost:3000/')
-    cy.wait(500);
+    cy.get('[data-cy=notificationOrderId]').eq(0).contains(4);
+    cy.get('[data-cy=notificationDate]').eq(1).contains('2023-04-21');
+    cy.get('[data-cy=notificationMessage]').eq(1).contains('Your order has been processed');
+    cy.get('[data-cy=notificationStatus]').eq(1).contains('read');
+    cy.get('[data-cy=notificationOrderId]').eq(1).contains(5);
+  })
+
+  it('test notification order 5 navigate from notifications overview',()=>{
     cy.visit('http://localhost:3000/notifications');
     cy.wait(500);
-    cy.get('[data-cy=notificationStatus]').eq(0).contains('unread');
+    cy.get('[data-cy=notification]').eq(0).click();
+    cy.wait(500);
+    cy.url().should('equal', 'http://localhost:3000/orders/4');
   })
 });
