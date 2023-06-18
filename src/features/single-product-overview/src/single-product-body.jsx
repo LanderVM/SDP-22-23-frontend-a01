826b0deb4c7410c65deb7cfd64eb8264861efac4
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-06-18 17:25:42 +0200

    Add in stock / notify stuff

--- src/features/single-product-overview/src/single-product-body.jsx
+++ src/features/single-product-overview/src/single-product-body.jsx
@@ -3,7 +3,9 @@ import { NavLink } from 'react-router-dom';
 import {
   Row, Col, Grid, Button,
 } from 'antd';
-import { CheckOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import {
+  CheckOutlined, ClockCircleOutlined, MailOutlined, ShopOutlined, ShoppingCartOutlined, WarningOutlined,
+} from '@ant-design/icons';
 import { ShoppingCartProducts } from '../../../contexts/shopping-cart-products';
 import '../single-product-overview.css';
 import ToastNotification from '../../../Components/notification';
@@ -18,9 +20,10 @@ export default function SingleProductBody({
   } = useContext(ShoppingCartProducts);
 
   const { md } = useBreakpoint();
-  const [notificationVisible, setNotificationVisible] = useState(false);
+  const [notificationVisible, setNotificationVisible] = useState({ status: false, isAdd: false });
   const imageMobileFormat = md ? 12 : 24;
-  const fontSizeIcon = md ? '170%' : '120%';
+  const fontSizeAddToCartIcon = md ? '170%' : '120%';
+  const fontSizeNotifyIcon = md ? '150%' : '100%';
   const buttonHeight = md ? '60px' : '40px';
   const paddingTextMF = md ? '3% 5%' : '0';
   const fontSizeTitle = md ? '40px' : '30px';
@@ -35,38 +38,55 @@ export default function SingleProductBody({
   }
 
   const handleAdd = () => {
-    addProductToShoppingCartContext(product, parseInt(productAmount, 10) + 1);
     const doIt = () => {
       addProductToShoppingCartContext(product, parseInt(productAmount, 10) + 1);
-      setNotificationVisible(true);
-      setTimeout(() => setNotificationVisible(false), 200);
+      setNotificationVisible({ status: true, isAdd: true });
+      setTimeout(() => setNotificationVisible({ status: true, isAdd: true }), 200);
+    };
+    doIt();
+  };
+
+  const handleNotify = () => {
+    const doIt = () => {
+      setNotificationVisible({ status: true, isAdd: false });
+      setTimeout(() => setNotificationVisible({ status: false, isAdd: false }), 200);
     };
     doIt();
   };
 
   const toastNotification = (
     <ToastNotification
-      title="Product Added"
-      message={
-                (
-                  <>
-                    <span style={{ fontWeight: 'bold' }}>{product.name}</span>
-                        &nbsp;has been added to your cart.
-                    <br />
-                    New total:
-                    {' '}
-                    {parseInt(productAmount, 10)}
-                  </>
-                )
-            }
-      icon={(
+      title={notificationVisible.isAdd ? 'Product Added' : 'Notify Enabled'}
+      message={notificationVisible.isAdd ? (
+        <>
+          <span style={{ fontWeight: 'bold' }}>{product.name}</span>
+              &nbsp;has been added to your cart.
+          <br />
+          New total:
+          {' '}
+          {parseInt(productAmount, 10)}
+        </>
+      ) : (
+        <>
+          You will be notified when&nbsp;
+          <span style={{ fontWeight: 'bold' }}>{product.name}</span>
+              &nbsp;will be in stock again.
+        </>
+      )}
+      icon={notificationVisible.isAdd ? (
         <ShoppingCartOutlined
           style={{
             color: '#ff4d4f',
           }}
         />
-            )}
-      show={notificationVisible}
+      ) : (
+        <MailOutlined
+          style={{
+            color: '#ff4d4f',
+          }}
+        />
+      )}
+      show={notificationVisible.status}
       updatable
       updatableKey={product.product_id}
     />
@@ -98,31 +118,101 @@ export default function SingleProductBody({
               </b>
             </p>
             <p style={{ fontSize: fontSizeDetails, color: product.stock > 0 ? 'green' : 'red', margin: marginDetails }} data-cy="productStock">
-              {product.stock > 0 ? `in stock (${product.stock} left)` : 'Out of stock'}
+              {product.stock > 0
+                ? (
+                  <ShopOutlined style={{
+                    color: '#ff4d4f',
+                  }}
+                  />
+                ) : (
+                  <WarningOutlined style={{
+                    color: '#ff4d4f',
+                  }}
+                  />
+                ) }
+              {product.stock > 0 ? `  ${product.stock} units available` : '  Out of Stock'}
             </p>
-            <Button
-              type="primary"
-              danger
-              data-cy="btnAddToCart"
-              onClick={(e) => {
-                e.preventDefault();
-                handleAdd();
-              }}
-              style={{
-                fontSize: fontSizeIcon, height: buttonHeight, verticalAlign: '10px', margin: marginDetails,
-              }}
-            >
-              <ShoppingCartOutlined style={{ fontSize: fontSizeIcon }} />
-              Add to cart
-            </Button>
+            {product.stock > 0 ? (
+              <Button
+                type="primary"
+                danger
+                data-cy="btnAddToCart"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleAdd();
+                }}
+                style={{
+                  fontSize: fontSizeAddToCartIcon,
+                  height: buttonHeight,
+                  margin: marginDetails,
+                  width: '15vw',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <ShoppingCartOutlined style={{ fontSize: fontSizeAddToCartIcon }} />
+                <span style={{
+                  fontSize: '120%',
+                }}
+                >
+                  Add to cart
+                </span>
+              </Button>
+            ) : (
+              <Button
+                type="primary"
+                danger
+                data-cy="btnAddToCart"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleNotify();
+                }}
+                style={{
+                  fontSize: fontSizeAddToCartIcon,
+                  height: buttonHeight,
+                  margin: marginDetails,
+                  width: '15vw',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <MailOutlined style={{ fontSize: fontSizeNotifyIcon }} />
+                <span style={{
+                  fontSize: '120%',
+                }}
+                >
+                  Notify me
+                </span>
+              </Button>
+            )}
             <p style={{ fontSize: fontSizeSmall, margin: marginDetails }}>
-              <CheckOutlined />
+              <CheckOutlined style={{
+                color: '#ff4d4f',
+              }}
+              />
                           &nbsp; 2 year warranty
               <br />
-              <CheckOutlined />
+              <CheckOutlined style={{
+                color: '#ff4d4f',
+              }}
+              />
                           &nbsp; 2 weeks return policy
               <br />
-              Estimated delivery time:
+              {product.stock > 0 ? null : (
+                <>
+&nbsp; Estimated restock time:
+                  {' '}
+                  {product.delivery_time}
+                  <br />
+                </>
+              )}
+              <ClockCircleOutlined style={{
+                color: '#ff4d4f',
+              }}
+              />
+                &nbsp; Estimated delivery time:
               {' '}
               {product.delivery_time}
             </p>
